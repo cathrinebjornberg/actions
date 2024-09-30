6041cf9e2331970554d441344aab44645169ae6b
89catbjo <cathrine.bjornberg@live.se>
2024-09-30 13:26:01 +0200

    prettier

---
+++ navigator/RootStackNavigation.tsx
@@ -0,0 +1,12 @@
+import Home from "../screens/Home";
+
+export type PramaList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+export default function RootStackNavigation() {
+  return (
+  <RootStackNavigation.Screen name="Home" components={Home} />;
+
+}
